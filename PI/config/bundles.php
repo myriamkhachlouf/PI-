72b6d656bfabd3e263fdd7ada1f94f0cc16ec152
s7aba <mohammedaziz.shaba@esprit.tn>
2021-04-01 03:23:03 +0100

    i hate you all

--- PI/config/bundles.php
+++ PI/config/bundles.php
@@ -21,4 +21,5 @@ return [
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     Nucleos\DompdfBundle\NucleosDompdfBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    JMS\TranslationBundle\JMSTranslationBundle::class => ['all' => true],
 ];
